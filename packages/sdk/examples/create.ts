48f5d2ab7017b68a7ab46eaab4516e0678e233e6
paul launay <launay.p@gmail.com>
2024-11-18 22:27:38 +0100

    feat(sdk): initial setup

---
+++ packages/sdk/examples/create.ts
@@ -0,0 +1,3 @@
+import { create } from '../src'
+
+create()
---
+++ packages/sdk/src/create.ts
@@ -0,0 +1,6 @@
+import { getInitialPrice } from 'core'
+
+export function create() {
+  getInitialPrice(1)
+  console.log('create memecoin')
+}
