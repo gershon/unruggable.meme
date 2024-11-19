2f20bea35ba30caad32ad996edb5a1d82e9772ec
paul launay <launay.p@gmail.com>
2024-11-19 11:56:52 +0100

    feat(sdk): test factory

--- packages/sdk/src/create.ts
+++ packages/sdk/src/create.ts
@@ -1,6 +1,19 @@
-import { getInitialPrice } from 'core'
+/* eslint-disable import/no-unused-modules */
+import { Factory } from 'core'
+import { constants, RpcProvider } from 'starknet'
 
-export function create() {
-  getInitialPrice(1)
+export async function create() {
+  const provider = new RpcProvider({
+    nodeUrl: 'https://starknet-mainnet.public.blastapi.io',
+  })
+
+  const factory = new Factory({ provider, chainId: constants.StarknetChainId.SN_MAIN })
+
+  try {
+    await factory.getMemecoin('0x')
+  } catch (e) {
+    console.log('Error: shit happened')
+  }
+
   console.log('create memecoin')
 }
